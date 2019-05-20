053ee06645a79057cbb62d635115799a5dc071b2
Madi <madelyn.e.helton@gmail.com>
2019-05-20 05:07:39 -0500

    adding pictures to pop ups

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
+import resume from '../images/resumeWord.png'
+import resumeEdu from '../images/resumeEdu.png'
+import resumePicture from '../images/DSC_1168.jpg'
+import computerClose from '../images/computerClose.jpg'
 import meSmiling from '../images/meSmiling.jpg'
 
 class Main extends React.Component {
@@ -14,16 +16,18 @@ class Main extends React.Component {
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <h2 className="major">Resume</h2>
+          <span className="image main"><img src={resumePicture} alt="" /></span>
+          <span className="image fit">
+            <img src={resume} alt="" />
+            <img src={resumeEdu} alt="" />
+          </span>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
+          <span className="image main"><img src={computerClose} alt="" /></span>
           <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
           <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
           {close}
@@ -32,11 +36,13 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={meSmiling} alt="" /></span>
+          <blockquote>
             <p>Front end developer who has simplifying the user experience in mind. Recently graduated from the UT coding bootcamp where I learned JavaScript, jQuery... etc.</p>
 
             <p>Starting my first job at 15 and working through high school and college helped to refine my work ethic and time management skills.</p> 
 
-            <p>Over 8 years in leadership positions, from being a customer service manager at a retail store to the store manager of a pet store. All of these positions were achieved because of my determination, leadership, problem solving, communication, and organizational skills.</p>
+            <p> Over 8 years in leadership positions, from being a customer service manager at a retail store to the store manager of a pet store. All of these positions were achieved because of my determination, leadership, problem solving, communication, and organizational skills.</p>
+            </blockquote>
           {close}
         </article>
 
